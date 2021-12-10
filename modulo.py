55f472b4172f3d5a7400f44965c1134686ab37b9
Ricardo Araújo <ricarlojo@gmail.com>
2021-12-10 23:40:27 +0000

    Update modulo.py
    está mal mas a ideia está aí, falta saber como o stor fez

--- modulo.py
+++ modulo.py
@@ -2,7 +2,14 @@ import json
 
 def lerficheiro(nome):
     ficheiro = open(nome, encoding="utf-8")
-    bd=[]
     dicJSON = json.load(ficheiro)
     return dicJSON
 
+def guardarficheiro(BD,nficheiro):
+    ficheiro = open(nficheiro,"w",encoding="utf-8")
+    ficheiro = json.dump
+    (BD)
+
+BD = ["Bonita vista!"]
+
+guardarficheiro(BD,"cinemaATP.json")

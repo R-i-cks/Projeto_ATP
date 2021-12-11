8b127590ab731fcf1884de81f17168f2da9268b6
monicaccmartins <monicacarina.martins@gmail.com>
2021-12-11 22:16:17 +0000

    Added "listarGenero" and "listarAlfabeticamente" functions

--- modulo.py
+++ modulo.py
@@ -6,3 +6,30 @@ def lerficheiro(nome):
     dicJSON = json.load(ficheiro)
     return dicJSON
 
+
+#  ------ LISTAR ALBAFETICAMENTE ----------------
+
+def chaveOrd(d):
+    chave=d['title'].replace("(","")
+    chave=chave.replace(")","")
+    chave=chave.replace("\'","")
+    chave=chave.replace("/'","")
+    chave=chave.replace(".'","")
+    chave=chave.replace(":'","")
+    chave=chave.replace(",","")
+    return chave
+
+def listarAlfabeticamente(bd):
+    bd.sort(key=chaveOrd)
+    return bd        
+
+
+#  ----- LISTAR POR DETERMINHADO GENERO ----------
+
+def listarGenero(bd, genero):
+    novaBD=[]
+    for filme in bd:
+        for elem in filme['genres']:               # Como os elementos da chave 'genres' estao em lista, temos de a percorrer
+            if elem==genero:
+                novaBD.append(filme)
+    return novaBD
